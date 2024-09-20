040230b2940942d19012f101ecfe5f0bde4f1bfa
pranav-kalondia <pranavkalondia1977@gmail.com>
2024-09-20 15:48:49 +0530

    fourth commit

--- frontend/src/Component/page1/Page1.jsx
+++ frontend/src/Component/page1/Page1.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from 'react'
 import "./Page1.css";
 
 const Page1 = () => {
   return (
+    
     <div className="page1">
-      <div className="text">
-        <h2 className="topheading">RECENT LAUNCH</h2>
-        <h1 className="bottomheading">Transporter-11<br />Mission</h1>
-      </div>
-      <div className="wrapper">
-        <button><span>Rewatch</span></button>
-      </div>
+        <div className="text">
+          <h2 className='topheading'>RECENT LAUNCH</h2>
+          <h1 className='bottomheading'>Transporter-11<br/>Mission</h1>
+        </div>
+        <div classname="wrapper">
+  <button href="#"><span>Rewatch</span></button>
+</div>
     </div>
-  );
+  )
 }
 
-export default Page1;
+export default Page1
